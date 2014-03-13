7ac9404a3469b0809be7c8222436e9351848fa79
Daniel Larimer <dlarimer@invictus-innovations.com>
2014-03-13 00:17:22 -0400

    Basic start of btsx wallet

---
+++ libraries/btsx/include/bts/btsx/wallet.hpp
@@ -0,0 +1,23 @@
+#pragma once
+#include <bts/wallet/wallet.hpp>
+
+namespace bts { namespace btsx {
+   using namespace bts::blockchain;
+
+   namespace detail { class wallet_impl; }
+
+   class wallet : public bts::wallet::wallet 
+   {
+      public:
+         wallet();
+         ~wallet();
+
+      protected:
+         virtual void dump_output( const trx_output& out );
+         virtual void scan_output( const trx_output& out, const output_reference& ref, const bts::wallet::output_index& idx );
+
+      private:
+         std::unique_ptr<detail::wallet_impl> my;
+   };
+
+} } // bts::btsx
