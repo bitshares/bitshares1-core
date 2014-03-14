9929ac6c515283fd2da7d8b26dc24c583f31f865
Daniel Larimer <dlarimer@invictus-innovations.com>
2014-03-13 20:05:51 -0400

    update fc

---
+++ programs/bts_xt/main.cpp
@@ -0,0 +1,14 @@
+#include "client.hpp"
+
+int main( int argc, char** argv )
+{
+   try {
+      bts::xt::client xt_client;
+
+   } 
+   catch ( const fc::exception& e ) 
+   {
+
+   }
+   return 0;
+}
