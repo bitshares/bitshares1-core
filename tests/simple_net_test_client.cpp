64ca4944de0979ca8cf2b45ae1bc648c02146aeb
Eric Frias <efrias@syncad.com>
2014-03-19 17:28:19 -0400

    Finish basic implementation of test program for bts::net::node

--- tests/simple_net_test_client.cpp
+++ tests/simple_net_test_client.cpp
@@ -8,7 +8,6 @@
 #include <bts/net/node.hpp>
 #include <bts/client/messages.hpp>
 
-#include <bts/wallet/wallet.hpp>
 #include <bts/blockchain/chain_database.hpp>
 #include <bts/blockchain/config.hpp>
 #include <fc/filesystem.hpp>
@@ -17,78 +16,132 @@
 #include <fc/reflect/variant.hpp>
 
 #include <iostream>
-using namespace bts::wallet;
+
 using namespace bts::blockchain;
+using namespace bts::net;
+using namespace bts::client;
 
-
-trx_block generate_genesis_block(const std::vector<address>& addr)
+// Generate a fake starting block.  
+// This block has all elements default-initialized, except for the block 
+// number which is 1.  (chosen because we'll consider a signature valid if 
+// it is equal to the block number, and we treat a signature of 0 to mean
+// no signature.
+block_message generate_fake_genesis_block()
 {
-    trx_block genesis;
-    genesis.version           = 0;
-    genesis.block_num         = 0;
-    genesis.timestamp         = fc::time_point::now();
-    genesis.next_fee          = block_header::min_fee();
-    genesis.total_shares      = 0;
-    genesis.votes_cast        = 0;
-    genesis.noncea            = 0;
-    genesis.nonceb            = 0;
-    genesis.next_difficulty   = 0;
-
-    signed_transaction trx;
-    for( uint32_t i = 0; i < addr.size(); ++i )
-    {
-        uint64_t amnt = rand()%1000 * BTS_BLOCKCHAIN_SHARE;
-        trx.outputs.push_back( trx_output( claim_by_signature_output( addr[i] ), asset( amnt ) ) );
-        genesis.total_shares += amnt;
-    }
-    genesis.available_votes   = genesis.total_shares;
-    genesis.trxs.push_back( trx );
-    genesis.trx_mroot = genesis.calculate_merkle_root(signed_transactions());
-
-    return genesis;
+  block_message genesis_block;
+  genesis_block.block.block_num = 1;
+  genesis_block.block_id = genesis_block.block.id();
+  genesis_block.signature.at(0) = 1;
+  return genesis_block;
 }
 
 
-using namespace bts::net;
-using namespace bts::client;
 using namespace boost::multi_index;
 
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+class blockchain_tied_message_cache 
+{
+private:
+  static const uint32_t cache_duration_in_blocks = 2;
+
+  struct message_hash_index{};
+  struct block_clock_index{};
+  struct message_info
+  {
+    message_hash_type message_hash;
+    message           message_body;
+    uint32_t          block_clock_when_received;
+    message_info(const message_hash_type& message_hash,
+                 const message&           message_body,
+                 uint32_t                 block_clock_when_received) :
+      message_hash(message_hash),
+      message_body(message_body),
+      block_clock_when_received (block_clock_when_received)
+    {}
+  };
+  typedef boost::multi_index_container<message_info, indexed_by<ordered_unique<tag<message_hash_index>, member<message_info, message_hash_type, &message_info::message_hash> >,
+                                                                ordered_non_unique<tag<block_clock_index>, member<message_info, uint32_t, &message_info::block_clock_when_received> > > > message_cache_container;
+  message_cache_container _message_cache;
+
+  uint32_t block_clock; //
+public:
+  blockchain_tied_message_cache() :
+    block_clock(0)
+  {}
+  void block_accepted();
+  void cache_message(const message& message_to_cache, const message_hash_type& hash_of_message_to_cache);
+  message get_message(const message_hash_type& hash_of_message_to_lookup);
+};
+
+void blockchain_tied_message_cache::block_accepted()
+{
+  ++block_clock;
+  if (block_clock > cache_duration_in_blocks)
+    _message_cache.get<block_clock_index>().erase(_message_cache.get<block_clock_index>().begin(), 
+                                                 _message_cache.get<block_clock_index>().lower_bound(block_clock - cache_duration_in_blocks));
+}
+
+void blockchain_tied_message_cache::cache_message(const message& message_to_cache, const message_hash_type& hash_of_message_to_cache)
+{
+  _message_cache.emplace(message_info(hash_of_message_to_cache, message_to_cache, block_clock));
+}
+
+message blockchain_tied_message_cache::get_message(const message_hash_type& hash_of_message_to_lookup)
+{
+  message_cache_container::index<message_hash_index>::type::const_iterator iter = _message_cache.get<message_hash_index>().find(hash_of_message_to_lookup);
+  if (iter != _message_cache.get<message_hash_index>().end())
+    return iter->message_body;
+  FC_THROW_EXCEPTION(key_not_found_exception, "Requested message not in cache");
+}
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
 class simple_net_test_miner : public bts::net::node_delegate
 {
 private:
-  struct block_hash{};
+  node _node;
+
+  // cache of messages we've seen in the past few blocks
+  blockchain_tied_message_cache _message_cache;
+
+  // blockchain is a series of block_messages
+  struct block_hash_index{};
   typedef boost::multi_index_container<block_message, indexed_by<random_access<>, 
-                                                                 ordered_unique<tag<block_hash>, member<block_message, block_id_type, &block_message::block_id> > 
-                                                                 ordered_unique<tag<message_hash>, member   > > ordered_blockchain_container;
-  ordered_blockchain_container blockchain;
+                                                                 ordered_unique<tag<block_hash_index>, member<block_message, block_id_type, &block_message::block_id> > > > ordered_blockchain_container;
+  ordered_blockchain_container _blockchain;
 
-  typedef boost::multi_index_container<block_message, indexed_by<ordered_unique<member<block_message, block_id_type, &block_message::block_id> > > > blockchain_container;
-  blockchain_container unsigned_blocks;
+  // the list of unsigned blocks we're considering adding to our chain
+  typedef boost::multi_index_container<block_message, indexed_by<ordered_unique<member<block_message, block_id_type, &block_message::block_id> > > > block_container;
+  block_container _unsigned_blocks;
 
-  struct transaction_hash{};
-  struct chain_length{};
+  // the list of transactions we're considering adding to a new block if we mine one.
+  // we expire these after a few blocks.  A real client would also expire them immediately
+  // if it received a block containing the transaction
+  struct transaction_hash_index{};
+  struct chain_length_index{};
   struct transaction_with_timestamp
   {
-    trx_message transaction_message;
-    transaction_id_type transaction_id;
-    uint32_t chain_length_when_received;
+    bts::blockchain::signed_transaction transaction;
+    transaction_id_type                 transaction_id;
+    uint32_t                            chain_length_when_received;
   };
-  typedef boost::multi_index_container<transaction_with_timestamp, indexed_by<ordered_unique<tag<transaction_hash>, member<transaction_with_timestamp, transaction_id_type, &transaction_with_timestamp::transaction_id> >, 
-                                                                              ordered_non_unique<tag<chain_length>, member<transaction_with_timestamp, uint32_t, &transaction_with_timestamp::chain_length_when_received> > > > transaction_container;
-  transaction_container transactions;
+  typedef boost::multi_index_container<transaction_with_timestamp, indexed_by<ordered_unique<tag<transaction_hash_index>, member<transaction_with_timestamp, transaction_id_type, &transaction_with_timestamp::transaction_id> >, 
+                                                                              ordered_non_unique<tag<chain_length_index>, member<transaction_with_timestamp, uint32_t, &transaction_with_timestamp::chain_length_when_received> > > > transaction_container;
+  transaction_container _transactions;
 
-  struct signature_hash{};
-  typedef boost::multi_index_container<signature_message, indexed_by<ordered_unique<tag<transaction_hash>, member<transaction_with_timestamp, transaction_id_type, &transaction_with_timestamp::transaction_id> >, 
-                                                                              ordered_non_unique<tag<chain_length>, member<transaction_with_timestamp, uint32_t, &transaction_with_timestamp::chain_length_when_received> > > > transaction_container;
-  
+  // the list of signatures we've seen but have been unable to pair with a block (maybe the unsigned block hasn't arrived yet).
+  typedef boost::multi_index_container<signature_message, indexed_by<ordered_unique<member<signature_message, block_id_type, &signature_message::block_id> > > > signature_container;
+  signature_container _signatures;
 
-  bool synchronizing;
+  bool _synchronized;
+  uint32_t _remaining_items_to_sync;
+
   void push_valid_signed_block(const block_message& block_to_push);
   bool is_block_valid(const block_message& block_to_check);
   void store_transaction(const trx_message& transaction_to_store);
 public:
+  simple_net_test_miner();
+  void run();
 
-
+  /* Implement node_delegate */
   bool has_item(const item_id& id) override;
   void handle_message(const message&) override;
   std::vector<item_hash_t> get_item_ids(const item_id& from_id, 
@@ -99,24 +152,53 @@ public:
   void connection_count_changed(uint32_t c) override;
 };
 
+simple_net_test_miner::simple_net_test_miner() :
+  _synchronized(false),
+  _remaining_items_to_sync(0)
+{
+  _node.set_delegate(this);
+  push_valid_signed_block(generate_fake_genesis_block());
+}
+
+void simple_net_test_miner::run()
+{
+  assert(!_blockchain.empty());
+  _synchronized = false;
+  _node.sync_from(item_id(block_message_type, _blockchain.get<0>().back().block_id));
+  // run some sort of event loop here
+}
 
 void simple_net_test_miner::push_valid_signed_block(const block_message& block_to_push)
 {
-  blockchain.push_back(block_to_push);
+  _blockchain.emplace_back(block_to_push);
+
   // any unsigned blocks we have are garbage
-  unsigned_blocks.clear();
-  // prune old transactions from memory
-  if (blockchain.size() > 2)
-    transactions.get<chain_length>().erase(transactions.get<chain_length>().begin(), transactions.get<chain_length>().lower_bound(blockchain.size() - 2));
+  _unsigned_blocks.clear();
+
+  // prune list of cached messages
+  _message_cache.block_accepted();
+  
+  // prune list of uncommitted _transactions
+  unsigned cached_transaction_duration_in_blocks = 2;
+  if (_blockchain.size() > cached_transaction_duration_in_blocks)
+    _transactions.get<chain_length_index>().erase(_transactions.get<chain_length_index>().begin(), 
+                                                 _transactions.get<chain_length_index>().lower_bound(_blockchain.size() - cached_transaction_duration_in_blocks));
+
+  if (!_synchronized)
+  {
+    --_remaining_items_to_sync;
+    if (_remaining_items_to_sync == 0)
+      _synchronized = true;
+  }
 }
 
 bool simple_net_test_miner::is_block_valid(const block_message& block_to_check)
 {
-  if (blockchain.empty())
+  if (_blockchain.empty())
     return true; // no way of knowing 
   block_to_check.block.prev;
-  ordered_blockchain_container::index<block_hash>::type::iterator iter = blockchain.get<block_hash>().find(block_to_check.block.prev);
-  if (iter != blockchain.get<block_hash>().end())
+  ordered_blockchain_container::index<block_hash_index>::type::iterator iter = _blockchain.get<block_hash_index>().find(block_to_check.block.prev);
+  if (iter != _blockchain.get<block_hash_index>().end())
     return true;
   return false;
 }
@@ -124,19 +206,18 @@ bool simple_net_test_miner::is_block_valid(const block_message& block_to_check)
 void simple_net_test_miner::store_transaction(const trx_message& transaction_to_store)
 {
   transaction_with_timestamp transaction_info;
-  transaction_info.transaction_message = transaction_to_store;
+  transaction_info.transaction = transaction_to_store.trx;
   transaction_info.transaction_id = transaction_to_store.trx.id(); 
-  transaction_info.chain_length_when_received = blockchain.size();
-  transactions.insert(transaction_info);
+  transaction_info.chain_length_when_received = _blockchain.size();
+  _transactions.insert(transaction_info);
 }
-
 
 bool simple_net_test_miner::has_item(const item_id& id)
 {
   if (id.item_type == trx_message_type)
-    return transactions.get<transaction_hash>().find(id.item_hash) != transactions.end();
+    return _transactions.get<transaction_hash_index>().find(id.item_hash) != _transactions.end();
   if (id.item_type == block_message_type)
-      return blockchain.get<block_hash>().find(id.item_hash) != blockchain.get<block_hash>().end();
+      return _blockchain.get<block_hash_index>().find(id.item_hash) != _blockchain.get<block_hash_index>().end();
   return false;
 }
 
@@ -152,7 +233,19 @@ void simple_net_test_miner::handle_message(const message& message_to_handle)
       if (block_message_to_handle.signature != fc::ecc::compact_signature())
         push_valid_signed_block(block_message_to_handle);
       else
-        unsigned_blocks.insert(block_message_to_handle);
+      {
+        // do I have a signature for it?
+        signature_container::iterator iter = _signatures.find(block_message_to_handle.block_id);
+        if (iter != _signatures.end())
+        {
+          // it's signed!
+          block_message_to_handle.signature = iter->signature;
+          _signatures.erase(iter);
+          push_valid_signed_block(block_message_to_handle);
+        }
+        else
+          _unsigned_blocks.insert(block_message_to_handle);
+      }
       break;
     }
   case trx_message_type:
@@ -164,26 +257,40 @@ void simple_net_test_miner::handle_message(const message& message_to_handle)
   case signature_message_type:
     {
       signature_message signature_message_to_handle(message_to_handle.as<signature_message>());
-
+      // do we have a block for it?
+      block_container::iterator iter = _unsigned_blocks.find(signature_message_to_handle.block_id);
+      if (iter != _unsigned_blocks.end())
+      {
+        block_message signed_block_message = *iter;
+        _unsigned_blocks.erase(iter);
+        signed_block_message.signature = signature_message_to_handle.signature;
+        push_valid_signed_block(signed_block_message);
+      }
+      else
+        _signatures.insert(signature_message_to_handle);
+      break;
     }
   }
+
+  // only cache the message if it was valid (we didn't throw)
+  _message_cache.cache_message(message_to_handle, message_to_handle.id());
 }
 
 std::vector<item_hash_t> simple_net_test_miner::get_item_ids(const item_id& from_id, 
                                                              uint32_t& remaining_item_count,
                                                              uint32_t limit)
 {
-  ordered_blockchain_container::index<block_hash>::type::iterator block_hash_iter = blockchain.get<block_hash>().find(from_id.item_hash);
-  if (block_hash_iter == blockchain.get<block_hash>().end())
+  ordered_blockchain_container::index<block_hash_index>::type::iterator block_hash_iter = _blockchain.get<block_hash_index>().find(from_id.item_hash);
+  if (block_hash_iter == _blockchain.get<block_hash_index>().end())
   {
     assert(false); // should this ever happen?
     FC_THROW("I've never seen your last item");
     remaining_item_count = 0;
     return std::vector<item_hash_t>();
   }
-  ordered_blockchain_container::nth_index<0>::type::iterator sequential_iter = blockchain.project<0>(block_hash_iter);
+  ordered_blockchain_container::nth_index<0>::type::iterator sequential_iter = _blockchain.project<0>(block_hash_iter);
   ++sequential_iter;
-  remaining_item_count = blockchain.get<0>().end() - sequential_iter;
+  remaining_item_count = _blockchain.get<0>().end() - sequential_iter;
   uint32_t items_to_get_this_iteration = std::min(limit, remaining_item_count);
   std::vector<item_hash_t> hashes_to_return;
   hashes_to_return.reserve(items_to_get_this_iteration);
@@ -198,22 +305,39 @@ std::vector<item_hash_t> simple_net_test_miner::get_item_ids(const item_id& from
 
 message simple_net_test_miner::get_item(const item_id& id) 
 {
-  if (id.item_type == trx_message_type)
+  try
   {
-    transaction_container::index<transaction_hash>::type::iterator iter = transactions.get<transaction_hash>().find(id.item_hash);
-    if (iter != transactions.get<transaction_hash>().end())
-      return iter->transaction_message;
+    return _message_cache.get_message(id.item_hash);
   }
+  catch (const fc::key_not_found_exception&)
+  {
+    // not in our cache.  Either it has already expired from our cache, or
+    // it's a request for an actual block during synchronization.  
+  }
+
   if (id.item_type == block_message_type)
   {
-    ordered_blockchain_container::index<block_hash>::type::iterator iter = blockchain.get<block_hash>().find(id.item_hash);
-    if (iter != blockchain.get<block_hash>().end())
+    ordered_blockchain_container::index<block_hash_index>::type::iterator iter = _blockchain.get<block_hash_index>().find(id.item_hash);
+    if (iter != _blockchain.get<block_hash_index>().end())
       return *iter;
   }
-  FC_THROW("I don't have the item you're looking for");
+
+  FC_THROW_EXCEPTION(key_not_found_exception, "I don't have the item you're looking for");
+}
+
+void simple_net_test_miner::sync_status(uint32_t item_type, uint32_t item_count)
+{
+  _remaining_items_to_sync = item_count;
+  _synchronized = _remaining_items_to_sync == 0;
+}
+
+void simple_net_test_miner::connection_count_changed(uint32_t c)
+{
 }
 
 int main()
 {
+  simple_net_test_miner miner;
+  miner.run();
   return 0;
 }
