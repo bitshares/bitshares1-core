4c7f2f004c259cfd5b4bae981147f9fc9db259e5
Vikram Rajkumar <vikramrajkumar1@gmail.com>
2014-03-22 11:24:56 -0400

    Add sell_domain_sig_fail unit test

--- tests/dns_tests.cpp
+++ tests/dns_tests.cpp
@@ -730,7 +730,60 @@ BOOST_AUTO_TEST_CASE( sell_domain )
  */
 BOOST_AUTO_TEST_CASE( sell_domain_sig_fail )
 {
+    bool fail = false; // what is best practice for 
+    try {
+        DNSTestState state;
+        state.normal_genesis();
 
+        bts::dns::dns_wallet* wallet = state.get_wallet();
+        dns_db* db = state.get_db();
+        std::vector<signed_transaction> txs;
+
+        std::string name = "TESTNAME";
+
+        // Create second wallet
+        fc::temp_directory dir;
+        bts::dns::dns_wallet wallet2;
+        wallet2.create( dir.path() / "dns_test_wallet2.dat", "password", "password", true );
+
+        // Give second wallet a balance
+        uint64_t amnt = rand()%1000 * BTS_BLOCKCHAIN_SHARE;
+        auto transfer_tx = wallet->transfer(asset(amnt), wallet2.new_recv_address());
+        wlog( "transfer_trx: ${trx} ", ("trx",transfer_tx) );
+        txs.push_back( transfer_tx );
+        state.next_block( txs );
+        txs.clear();
+
+        // Initial domain purchase
+        uint64_t bid1 = 1;
+        auto buy_tx = wallet->buy_domain( name, asset(bid1), *db );
+        wlog( "buy_trx: ${trx} ", ("trx",buy_tx) );
+        txs.push_back( buy_tx );
+        state.next_block( txs );
+        txs.clear();
+
+        // Let auction expire
+        for (auto i = 0; i < DNS_AUCTION_DURATION_BLOCKS; i++)
+            state.next_block(txs); 
+
+        // Attempt to sell record from second wallet
+        wallet2.scan_chain(*state.get_db());
+        auto sell_tx = wallet2.sell_domain(name, asset(bid1), *db);
+        wlog( "sell_trx: ${trx} ", ("trx", sell_tx) );
+        txs.push_back( sell_tx );
+        state.next_block( txs );
+
+        fail = true;
+        FC_ASSERT(0);
+    }
+    catch (const fc::exception& e)
+    {
+        if (fail)
+        {
+            elog( "${e}", ("e",e.to_detail_string()) );
+            throw;
+        }
+    }
 }
 
 /* You should not be able to sell a domain if you don't own it (expired)
