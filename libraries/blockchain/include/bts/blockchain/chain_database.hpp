e00e3d006b6e8096a091c9794f1abab3b0fdd252
Vikram Rajkumar <vikramrajkumar1@gmail.com>
2014-04-09 14:08:35 -0400

    Allow chain server to be used with derived databases

--- libraries/blockchain/include/bts/blockchain/chain_database.hpp
+++ libraries/blockchain/include/bts/blockchain/chain_database.hpp
@@ -4,7 +4,7 @@
 #include <bts/blockchain/transaction_validator.hpp>
 #include <bts/blockchain/pow_validator.hpp>
 
-namespace fc 
+namespace fc
 {
    class path;
 };
@@ -14,35 +14,35 @@ namespace bts { namespace blockchain {
     namespace detail  { class chain_database_impl; }
 
     /**
-     *  @class chain_database 
+     *  @class chain_database
      *  @ingroup blockchain
      *
-     *  The chain_database provides the generic implementation of basic 
+     *  The chain_database provides the generic implementation of basic
      *  blockchain semantics which requires that blocks can be pushed,
-     *  popped and that all transactions are validated.  
+     *  popped and that all transactions are validated.
      *
      *  There can be many variations on the chain_database each of which
      *  implements custom logic for handling different transactions.
      *
      *  This database only stores valid blocks and applied transactions,
      *  it does not store invalid/orphaned blocks and transactions which
-     *  are maintained in a separate database 
+     *  are maintained in a separate database
      */
-    class chain_database 
+    class chain_database
     {
        protected:
           /**
            *  This method should perform the validation step of making sure that the block and
-           *  all associated  determinsitc transactions can be applied.  It should throw an
+           *  all associated deterministic transactions can be applied. It should throw an
            *  exception of the validation fails.
            */
-          virtual void validate( const trx_block& blk, const signed_transactions& determinsitc_trxs );
+          virtual void validate( const trx_block& blk, const signed_transactions& deterministic_trxs );
 
-          /** 
+          /**
            *  Called after a block has been validated and appends
            *  it to the block chain storing all relevant transactions.
            */
-          virtual void store( const trx_block& blk, const signed_transactions& determinsitc_trxs );
+          virtual void store( const trx_block& blk, const signed_transactions& deterministic_trxs );
 
 
        public:
@@ -51,7 +51,7 @@ namespace bts { namespace blockchain {
 
           /**
            *  There are many kinds of deterministic transactions that various blockchains may require
-           *  such as automatic inactivity fees, lottery winners, and market making.   This method
+           *  such as automatic inactivity fees, lottery winners, and market making. This method
            *  can be overloaded to
            */
           virtual signed_transactions generate_deterministic_transactions();
@@ -62,11 +62,11 @@ namespace bts { namespace blockchain {
           //@{
           /**
            *  The signing authority is a key that signs every block
-           *  once they have successfully pushed it to their chain.  
+           *  once they have successfully pushed it to their chain.
            *
            *  The purpose of the signing authority is to make sure that
            *  there are no chain forks until the consensus algorithm can
-           *  be reached.  
+           *  be reached.
            */
           void                        set_trustee( const address& addr );
           address                     get_trustee()const;
@@ -82,14 +82,14 @@ namespace bts { namespace blockchain {
           void set_transaction_validator( const transaction_validator_ptr& v );
           transaction_validator_ptr get_transaction_validator()const;
 
-          void open( const fc::path& dir, bool create = true );
-          void close();
+          virtual void open( const fc::path& dir, bool create = true );
+          virtual void close();
 
           const signed_block_header&  get_head_block()const;
           uint64_t                    total_shares()const;
           uint32_t                    head_block_num()const;
           block_id_type               head_block_id()const;
-          uint64_t                    get_stake(); // head - 1 
+          uint64_t                    get_stake(); // head - 1
           asset                       get_fee_rate()const;
 
 
@@ -115,14 +115,14 @@ namespace bts { namespace blockchain {
          void push_block( const trx_block& b );
 
          /**
-          *  Removes the top block from the stack and marks all spent outputs as 
+          *  Removes the top block from the stack and marks all spent outputs as
           *  unspent.
           */
          virtual trx_block pop_block();
 
        private:
          void   store_trx( const signed_transaction& trx, const trx_num& t );
-         std::unique_ptr<detail::chain_database_impl> my;          
+         std::unique_ptr<detail::chain_database_impl> my;
     }; // chain_database
 
     typedef std::shared_ptr<chain_database> chain_database_ptr;
