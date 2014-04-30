ffda8835578bdda912a6121c3cac460737208aa4
Vikram Rajkumar <vikramrajkumar1@gmail.com>
2014-04-30 16:18:57 -0400

    Fix compilation

--- libraries/wallet/include/bts/wallet/delegate_status.hpp
+++ libraries/wallet/include/bts/wallet/delegate_status.hpp
@@ -3,12 +3,12 @@
 
 namespace bts { namespace wallet {
 
-      enum trust_state
+      enum trust_state_enum
       {
          untrusted,  ///< no user provided trust information
          trusted,    ///< user has indicated trust
          distrusted, ///< user has indiciated distrust
-         fired       ///< delegate has been fired for a provable offense   
+         fired       ///< delegate has been fired for a provable offense
       };
    struct delegate_status : public bts::blockchain::name_record
    {
@@ -21,8 +21,8 @@ namespace bts { namespace wallet {
        blocks_missed(0),
        transactions_included(0),
        transactions_excluded(0){}
-      
-      fc::enum_type<trust_state,uint8_t> trust_state;
+
+      fc::enum_type<trust_state_enum,uint8_t> trust_state;
       uint32_t                           blocks_produced;
       uint32_t                           blocks_missed;
       uint32_t                           transactions_included;
@@ -31,8 +31,8 @@ namespace bts { namespace wallet {
 
 } }
 
-FC_REFLECT_ENUM( bts::wallet::trust_state, (untrusted)(trusted)(distrusted)(fired) )
-FC_REFLECT( bts::wallet::delegate_status, 
+FC_REFLECT_ENUM( bts::wallet::trust_state_enum, (untrusted)(trusted)(distrusted)(fired) )
+FC_REFLECT( bts::wallet::delegate_status,
             (trust_state)
             (blocks_produced)
             (blocks_missed)
