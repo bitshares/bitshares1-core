8b9120749224e7f414eb61567755ba7fdc5b7b49
Vikram Rajkumar <vikramrajkumar1@gmail.com>
2014-05-15 01:39:56 -0400

    Delete old extended_address files

--- libraries/wallet/include/bts/wallet/delegate_status.hpp
+++ libraries/wallet/include/bts/wallet/delegate_status.hpp
@@ -8,7 +8,7 @@ namespace bts { namespace wallet {
       untrusted,  ///< no user provided trust information
       trusted,    ///< user has indicated trust
       distrusted, ///< user has indiciated distrust
-      fired       ///< delegate has been fired for a provable offense   
+      fired       ///< delegate has been fired for a provable offense
    };
 
    struct delegate_status : public bts::blockchain::name_record
@@ -22,8 +22,8 @@ namespace bts { namespace wallet {
        blocks_missed(0),
        transactions_included(0),
        transactions_excluded(0){}
-      
-      fc::enum_type<trust_state_enum,uint8_t> trust_state;
+
+      fc::enum_type<uint8_t,trust_state_enum> trust_state;
       uint32_t                                blocks_produced;
       uint32_t                                blocks_missed;
       uint32_t                                transactions_included;
@@ -33,7 +33,7 @@ namespace bts { namespace wallet {
 } }
 
 FC_REFLECT_ENUM( bts::wallet::trust_state_enum, (untrusted)(trusted)(distrusted)(fired) )
-FC_REFLECT( bts::wallet::delegate_status, 
+FC_REFLECT( bts::wallet::delegate_status,
             (trust_state)
             (blocks_produced)
             (blocks_missed)
