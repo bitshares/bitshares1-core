f965d65d7d095004af9b461cdae6f3fadc7e18fb
Eric Frias <efrias@syncad.com>
2014-05-27 19:17:07 -0400

    Generator now generating all five files (api, client, and server sides), just need a lot of little details implemented

--- libraries/api/bts_api_generator.cpp
+++ libraries/api/bts_api_generator.cpp
@@ -83,7 +83,7 @@ public:
 };
 typedef std::shared_ptr<dictionary_type_mapping> dictionary_type_mapping_ptr;
 
-struct parameter
+struct parameter_description
 {
   std::string name;
   type_mapping_ptr type;
@@ -91,15 +91,15 @@ struct parameter
   fc::optional<std::string> default_value;
   fc::optional<std::string> example;
 };
-typedef std::list<parameter> parameter_list;
+typedef std::list<parameter_description> parameter_description_list;
 
-struct method
+struct method_description
 {
   std::string name;
   type_mapping_ptr return_type;
-  parameter_list parameters;
+  parameter_description_list parameters;
 };
-typedef std::list<method> method_list;
+typedef std::list<method_description> method_description_list;
 
 class api_generator
 {
@@ -109,18 +109,21 @@ private:
   typedef std::map<std::string, type_mapping_ptr> type_map_type;
   type_map_type _type_map;
 
-  method_list _methods;
+  method_description_list _methods;
 public:
   api_generator(const std::string& classname); 
 
-  void load_api_description(const fc::path& api_description_file);
-  void generate_interface_file(const fc::path& api_description_file);
+  void load_api_description(const fc::path& api_description_filename);
+  void generate_interface_file(const fc::path& api_description_filename);
+  void generate_client_files(const fc::path& client_header_filename, const fc::path& client_cpp_filename);
+  void generate_server_files(const fc::path& server_header_filename, const fc::path& server_cpp_filename);
 private:
   type_mapping_ptr lookup_type_mapping(const std::string& type_string);
   void initialize_type_map_with_fundamental_types();
   void load_type_map(const fc::variants& json_type_map);
-  parameter_list load_parameters(const fc::variants& parameter_descriptions);
-  void load_method_descriptions(const fc::variants& method_descriptions);
+  parameter_description_list load_parameters(const fc::variants& json_parameter_descriptions);
+  void load_method_descriptions(const fc::variants& json_method_descriptions);
+  std::string generate_signature_for_method(const method_description& method, const std::string& class_name, bool include_default_parameters);
 };
 
 api_generator::api_generator(const std::string& classname) :
@@ -182,25 +185,25 @@ void api_generator::load_type_map(const fc::variants& json_type_map)
   }
 }
 
-parameter_list api_generator::load_parameters(const fc::variants& parameter_descriptions)
+parameter_description_list api_generator::load_parameters(const fc::variants& json_parameter_descriptions)
 {
-  parameter_list parameters;
-  for (const fc::variant& parameter_description_variant : parameter_descriptions)
+  parameter_description_list parameters;
+  for (const fc::variant& parameter_description_variant : json_parameter_descriptions)
   {
     fc::variant_object json_parameter_description = parameter_description_variant.get_object();
-    parameter parameter_description;
+    parameter_description parameter;
     FC_ASSERT(json_parameter_description.contains("name"));
-    parameter_description.name = json_parameter_description["name"].as_string();
+    parameter.name = json_parameter_description["name"].as_string();
     FC_ASSERT(json_parameter_description.contains("description"));
-    parameter_description.description = json_parameter_description["description"].as_string();
+    parameter.description = json_parameter_description["description"].as_string();
     FC_ASSERT(json_parameter_description.contains("type"));
-    parameter_description.type = lookup_type_mapping(json_parameter_description["type"].as_string());
+    parameter.type = lookup_type_mapping(json_parameter_description["type"].as_string());
     if (json_parameter_description.contains("default_value"))
-      parameter_description.default_value = json_parameter_description["default_value"].as_string();
+      parameter.default_value = json_parameter_description["default_value"].as_string();
     if (json_parameter_description.contains("example"))
-      parameter_description.example = json_parameter_description["example"].as_string();
+      parameter.example = json_parameter_description["example"].as_string();
 
-    parameters.push_back(parameter_description);
+    parameters.push_back(parameter);
   }
   return parameters;
 }
@@ -214,52 +217,186 @@ void api_generator::load_method_descriptions(const fc::variants& method_descript
     std::string method_name = json_method_description["method_name"].as_string();
     try
     {
-      method method_info;
-      method_info.name = method_name;
+      method_description method;
+      method.name = method_name;
       FC_ASSERT(json_method_description.contains("return_type"));
       std::string return_type_name = json_method_description["return_type"].as_string();
-      method_info.return_type = lookup_type_mapping(return_type_name);
+      method.return_type = lookup_type_mapping(return_type_name);
       
       FC_ASSERT(json_method_description.contains("parameters"));
-      method_info.parameters = load_parameters(json_method_description["parameters"].get_array());
+      method.parameters = load_parameters(json_method_description["parameters"].get_array());
 
-      _methods.push_back(method_info);
+      _methods.push_back(method);
     }
     FC_RETHROW_EXCEPTIONS(warn, "error encountered parsing method description for method \"${method_name}\"", ("method_name", method_name));
   }
 }
 
-void api_generator::load_api_description(const fc::path& api_description_file)
+void api_generator::load_api_description(const fc::path& api_description_filename)
 {
-  FC_ASSERT(fc::exists(api_description_file));
-  fc::variant_object api_description = fc::json::from_file(api_description_file).get_object();
+  FC_ASSERT(fc::exists(api_description_filename));
+  fc::variant_object api_description = fc::json::from_file(api_description_filename).get_object();
   if (api_description.contains("type_map"))
     load_type_map(api_description["type_map"].get_array());
   FC_ASSERT(api_description.contains("methods"));
   load_method_descriptions(api_description["methods"].get_array());
 }
 
-void api_generator::generate_interface_file(const fc::path& api_interface_file)
+std::string api_generator::generate_signature_for_method(const method_description& method, const std::string& class_name, bool include_default_parameters)
 {
-  std::ofstream interface_file(api_interface_file.string());
-  interface_file << "#pragma once\n\n";
-  interface_file << "namespace bts { namespace rpc {\n";
-
-  interface_file << "class << " << _api_classname << "\n";
-  interface_file << "{\n";
-  interface_file << "public:\n";
-
-  for (const method& method_description : _methods)
+  std::ostringstream method_signature;
+  method_signature << method.return_type->get_cpp_return_type() << " ";
+  if (!class_name.empty())
+    method_signature << class_name << "::";
+  method_signature << method.name << "(";
+  bool first_parameter = true;
+  for (const parameter_description& parameter : method.parameters)
   {
-    interface_file << method_description.return_type->get_cpp_return_type() << " " << method_description.name << "(";
-    for (const parameter& parameter_description : method_description.name)
+    if (first_parameter)
+      first_parameter = false;
+    else
+      method_signature << ", ";
+    method_signature << parameter.type->get_cpp_parameter_type() << " " << parameter.name;
+    if (parameter.default_value)
     {
-      
+      if (!include_default_parameters)
+        method_signature << " /*";
+      method_signature << " = " << *parameter.default_value;
+      if (!include_default_parameters)
+        method_signature << " */";
     }
   }
+  method_signature << ")";
+  return method_signature.str();
+}
 
-  interface_file << "}\n\n";
-  interface_file << "} } // end namespace bts::rpc\n";
+void api_generator::generate_interface_file(const fc::path& api_interface_filename)
+{
+  std::ofstream interface_file(api_interface_filename.string());
+  interface_file << "#pragma once\n\n";
+  interface_file << "namespace bts { namespace api {\n\n";
+
+  interface_file << "  class " << _api_classname << "\n";
+  interface_file << "  {\n";
+  interface_file << "  public:\n";
+
+  for (const method_description& method : _methods)
+  {
+    interface_file << "    " << generate_signature_for_method(method, "", true) << " = 0;\n";
+  }
+
+  interface_file << "  };\n\n";
+  interface_file << "} } // end namespace bts::api\n";
+}
+
+void api_generator::generate_client_files(const fc::path& client_header_filename, const fc::path& client_cpp_filename)
+{
+  std::ofstream header_file(client_header_filename.string());
+  std::ofstream cpp_file(client_cpp_filename.string());
+
+  std::string client_classname = _api_classname + "_client";
+
+  header_file << "#pragma once\n\n";
+  header_file << "namespace bts { namespace rpc {\n\n";
+  header_file << "  class " << client_classname << " : public bts::api::" << _api_classname << "\n";
+  header_file << "  {\n";
+  header_file << "  public:\n";
+
+  for (const method_description& method : _methods)
+  {
+    header_file << "    " << generate_signature_for_method(method, "", true) << " override;\n";
+  }
+
+  header_file << "  };\n\n";
+  header_file << "} } // end namespace bts::rpc\n";
+
+
+  cpp_file << "#include <header_file>\n";
+  cpp_file << "namespace bts { namespace rpc {\n\n";
+
+  for (const method_description& method : _methods)
+  {
+    cpp_file << generate_signature_for_method(method, client_classname, false) << "\n";
+    cpp_file << "{\n";
+
+    if (std::dynamic_pointer_cast<void_type_mapping>(method.return_type))    
+      cpp_file << "  _json_connection->async_call(";
+    else
+      cpp_file << "  return _json_connection->call<" << method.return_type->get_cpp_return_type() << ">(";
+    cpp_file << "\"" << method.name << "\"";
+    for (const parameter_description& parameter : method.parameters)
+      cpp_file << ", " << parameter.name;
+    cpp_file << ")";
+    if (std::dynamic_pointer_cast<void_type_mapping>(method.return_type))    
+      cpp_file << ".wait()";
+    cpp_file << ";\n";
+    cpp_file << "}\n";
+  }
+  cpp_file << "\n";
+  cpp_file << "} } // end namespace bts::rpc\n";
+}
+
+void api_generator::generate_server_files(const fc::path& client_header_filename, const fc::path& client_cpp_filename)
+{
+  std::ofstream header_file(client_header_filename.string());
+  std::ofstream cpp_file(client_cpp_filename.string());
+
+  std::string server_classname = _api_classname + "_server";
+
+  header_file << "#pragma once\n\n";
+  header_file << "namespace bts { namespace rpc {\n\n";
+  header_file << "  class " << server_classname << "\n";
+  header_file << "  {\n";
+  header_file << "  public:\n";
+
+  for (const method_description& method : _methods)
+  {
+    header_file << "    fc::variant " << method.name << "_positional(const fc::variants& parameters);\n";
+    header_file << "    fc::variant " << method.name << "_named(const fc::variant_object& parameters);\n";
+  }
+
+  header_file << "  };\n\n";
+  header_file << "} } // end namespace bts::rpc\n";
+
+  cpp_file << "#include <header_file>\n";
+  cpp_file << "namespace bts { namespace rpc {\n\n";
+
+  for (const method_description& method : _methods)
+  {
+    cpp_file << "fc::variant " << server_classname << "::" << method.name << "_positional(const fc::variants& parameters)\n";
+    cpp_file << "{\n";
+    unsigned parameter_index = 0;
+    for (const parameter_description& parameter : method.parameters)
+    {
+      if (!parameter.default_value)
+      {
+        // parameter is required
+        cpp_file << "  if (parameters.size() <= " << parameter_index << ")\n";
+        cpp_file << "    FC_THROW_EXCEPTION(invalid_arg_exception, \"missing required parameter " << (parameter_index + 1) << " (" << parameter.name << ")\");\n";
+        cpp_file << "  " << parameter.type->get_cpp_return_type() << " " << parameter.name << 
+                    " = parameters[" << parameter_index << "].as<" << parameter.type->get_cpp_return_type() << ">();\n";
+      }
+      ++parameter_index;
+    }
+
+    cpp_file << "  ";
+    if (!std::dynamic_pointer_cast<void_type_mapping>(method.return_type))
+      cpp_file << "return";
+    cpp_file << "_client->" << method.name << "(";
+    bool first_parameter = true;
+    for (const parameter_description& parameter : method.parameters)
+    {
+      if (first_parameter)
+        first_parameter = false;
+      else
+        cpp_file << ", ";
+      cpp_file << parameter.name;
+    }
+    cpp_file << ");\n";
+    cpp_file << "}\n";
+  }
+  cpp_file << "\n";
+  cpp_file << "} } // end namespace bts::rpc\n";
 }
 
 void api_generator::initialize_type_map_with_fundamental_types()
@@ -277,6 +414,7 @@ void api_generator::initialize_type_map_with_fundamental_types()
   {
     "std::string",
   };
+
   _type_map.insert(type_map_type::value_type("void", std::make_shared<void_type_mapping>()));
   for (const char* by_value_type : pass_by_value_types)
     _type_map.insert(type_map_type::value_type(by_value_type, std::make_shared<fundamental_type_mapping>(by_value_type, by_value_type, by_value_type)));
@@ -337,6 +475,30 @@ int main(int argc, char*argv[])
   if (option_variables.count("api-interface-file"))
   {
     generator.generate_interface_file(option_variables["api-interface-file"].as<std::string>());
+  }
+
+  if (option_variables.count("rpc-client-cpp") ||
+      option_variables.count("rpc-client-hpp"))
+  {
+    if (!option_variables.count("rpc-client-cpp") || !option_variables.count("rpc-client-hpp"))
+    {
+      std::cout << "You must specify both --rpc-client-cpp and --rpc-client-hpp\n";
+      return 1;
+    }
+    generator.generate_client_files(option_variables["rpc-client-hpp"].as<std::string>(), 
+                                    option_variables["rpc-client-cpp"].as<std::string>());
+  }
+
+  if (option_variables.count("rpc-server-cpp") ||
+      option_variables.count("rpc-server-hpp"))
+  {
+    if (!option_variables.count("rpc-server-cpp") || !option_variables.count("rpc-server-hpp"))
+    {
+      std::cout << "You must specify both --rpc-server-cpp and --rpc-server-hpp\n";
+      return 1;
+    }
+    generator.generate_server_files(option_variables["rpc-server-hpp"].as<std::string>(), 
+                                    option_variables["rpc-server-cpp"].as<std::string>());
   }
 
   return 0;
