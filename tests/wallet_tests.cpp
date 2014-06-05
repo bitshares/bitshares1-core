ccabe4528b2d6332f5518e5e58ec98b0f75cbb04
Daniel Larimer <dlarimer@invictus-innovations.com>
2014-06-05 10:57:43 -0400

    Creating 2nd transfer mode, combined vs multi-part
    Support for transferring created assets.

--- tests/wallet_tests.cpp
+++ tests/wallet_tests.cpp
@@ -168,6 +168,9 @@ BOOST_AUTO_TEST_CASE( client_tests )
       your_cli->execute_command_line( "wallet_account_transaction_history youraccount" );
       your_cli->execute_command_line( "blockchain_list_registered_assets" );
       your_cli->execute_command_line( "balance" );
+      your_cli->execute_command_line( "transfer 2000000 USD otheraccount youraccount \"payday\"" );
+      produce_block( my_client );
+      your_cli->execute_command_line( "balance" );
       
       //ilog( "unspent:\n ${r}", ("r", fc::json::to_pretty_string(result)) );
 
