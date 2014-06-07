9ac32f1ea22507ce64760d584e15986490203f1c
Daniel Larimer <dlarimer@invictus-innovations.com>
2014-06-06 22:29:50 -0400

    fix duplicate exception code

--- libraries/wallet/include/bts/wallet/exceptions.hpp
+++ libraries/wallet/include/bts/wallet/exceptions.hpp
@@ -5,6 +5,6 @@ namespace bts { namespace wallet {
   FC_DECLARE_EXCEPTION( invalid_password,      20001, "Invalid Password" );
   FC_DECLARE_EXCEPTION( login_required,        20003, "Login Required" );
   FC_DECLARE_EXCEPTION( wallet_already_exists, 20004, "Wallet Already Exists" );
-  FC_DECLARE_EXCEPTION( no_such_wallet,        20004, "Wallet Does Not Exist" );
+  FC_DECLARE_EXCEPTION( no_such_wallet,        20005, "Wallet Does Not Exist" );
   // registered in wallet.cpp
 } } // bts::wallet
