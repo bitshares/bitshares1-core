2994145c58caa06f911308afae70255b5d4d7e93
Daniel Larimer <dlarimer@invictus-innovations.com>
2014-06-09 12:02:27 -0400

    support for derived exceptions

--- libraries/blockchain/include/bts/blockchain/exceptions.hpp
+++ libraries/blockchain/include/bts/blockchain/exceptions.hpp
@@ -2,9 +2,10 @@
 #include <fc/exception/exception.hpp>
 
 namespace bts { namespace blockchain {
-   FC_DECLARE_EXCEPTION( invalid_pts_address, 30001, "Invalid PTS Address" ); 
-   FC_DECLARE_EXCEPTION( addition_overflow,   30002, "Addition Overflow" ); 
-   FC_DECLARE_EXCEPTION( addition_underthrow, 30003, "Addition Underflow" ); 
-   FC_DECLARE_EXCEPTION( asset_type_mismatch, 30004, "Asset/Price Mismatch" ); 
+   FC_DECLARE_EXCEPTION( exception, 30000, "Blockchain Exception" ); 
+   FC_DECLARE_DERIVED_EXCEPTION( invalid_pts_address, bts::blockchain::exception, 30001, "Invalid PTS Address" ); 
+   FC_DECLARE_DERIVED_EXCEPTION( addition_overflow,   bts::blockchain::exception, 30002, "Addition Overflow" ); 
+   FC_DECLARE_DERIVED_EXCEPTION( addition_underthrow, bts::blockchain::exception, 30003, "Addition Underflow" ); 
+   FC_DECLARE_DERIVED_EXCEPTION( asset_type_mismatch, bts::blockchain::exception, 30004, "Asset/Price Mismatch" ); 
    // registered in blockchain.cpp 
 } }
