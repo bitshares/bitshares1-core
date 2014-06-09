00512dff5a717bc021a1f3fc3d0637fd777c5237
Daniel Larimer <dlarimer@invictus-innovations.com>
2014-06-08 20:44:59 -0400

    update how the qt_wallet builds

--- libraries/blockchain/include/bts/blockchain/config.hpp
+++ libraries/blockchain/include/bts/blockchain/config.hpp
@@ -7,6 +7,8 @@
 #define BTS_BLOCKCHAIN_VERSION                      (101)
 #define BTS_WALLET_VERSION                          (100)
 
+#define BTS_NETWORK_DEFAULT_P2P_PORT                8764
+
 /**
  *  The address prepended to string representation of
  *  addresses.
