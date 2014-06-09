c5f9a3ec8c3373970876716af6ded0b0c6b91cf1
Daniel Larimer <dlarimer@invictus-innovations.com>
2014-06-09 19:56:03 -0400

    enabling support for simulated time

--- libraries/blockchain/include/bts/blockchain/time.hpp
+++ libraries/blockchain/include/bts/blockchain/time.hpp
@@ -5,6 +5,7 @@ namespace bts { namespace blockchain {
 
 
    void               advance_time( int32_t seconds );
+   void               start_simulated_time( const fc::time_point& sim_time );
    fc::time_point_sec now();
 
 
