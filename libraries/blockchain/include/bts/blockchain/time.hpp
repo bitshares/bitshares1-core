1036cc0c44206413c2a2edd0770732899bfc7429
Daniel Larimer <dlarimer@invictus-innovations.com>
2014-06-16 13:53:59 -0400

    Ticket #285 - reporting NTP time and error in info

--- libraries/blockchain/include/bts/blockchain/time.hpp
+++ libraries/blockchain/include/bts/blockchain/time.hpp
@@ -1,12 +1,15 @@
 #pragma once
 #include <fc/time.hpp>
+#include <fc/optional.hpp>
 
 namespace bts { namespace blockchain {
 
 
-   void               advance_time( int32_t seconds );
-   void               start_simulated_time( const fc::time_point& sim_time );
-   fc::time_point_sec now();
+   fc::optional<fc::time_point> ntp_time();
+
+   void                   advance_time( int32_t seconds );
+   void                   start_simulated_time( const fc::time_point& sim_time );
+   fc::time_point_sec     now();
 
 
 } }
