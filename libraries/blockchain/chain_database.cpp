3c6e733dddfc016e270b58a2b468832ed81d9a68
Daniel Larimer <dlarimer@invictus-innovations.com>
2014-06-23 11:56:19 -0400

    block producers now get paid from fees alone

--- libraries/blockchain/chain_database.cpp
+++ libraries/blockchain/chain_database.cpp
@@ -1419,7 +1419,7 @@ namespace bts { namespace blockchain {
        *  Right now delegates are paid a salary regardless of fees, this initial salary is a pittance and
        *  should be less than eventual fees.
        */
-      total_fees = BTS_BLOCKCHAIN_BLOCK_REWARD;
+      // total_fees = BTS_BLOCKCHAIN_BLOCK_REWARD;
       next_block.delegate_pay_rate  = next_block.next_delegate_pay( my->_head_block_header.delegate_pay_rate, total_fees );
 
 
