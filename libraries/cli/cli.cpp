eacf57f52f537b39cdfdda8d08c840e0bcb6fc5b
Nathan Hourt <nathan@bitshares.org>
2014-07-09 11:31:13 -0400

    Resolve #480: Fix alignment issues in order book (for now at least)

--- libraries/cli/cli.cpp
+++ libraries/cli/cli.cpp
@@ -931,10 +931,10 @@ namespace bts { namespace cli {
               else if (method_name == "blockchain_market_order_book")
               {
                   auto bids_asks = result.as<std::pair<vector<market_order>,vector<market_order>>>();
-                  *_out << std::string(28, ' ') << "BIDS" << std::string(28, ' ') << " | " << std::string(28, ' ') << "ASKS" << std::string(28, ' ') << "\n"
-                        << std::left << std::setw(26) << "TOTAL" << std::setw(14) << "QUANTITY" << std::right << std::setw(20) << "PRICE"
-                        << " | " << std::left << std::setw(14) << "PRICE" << std::right << std::setw(20) << "QUANTITY" << std::setw(26) << "TOTAL" << "\n"
-                        << std::string(123, '-') << "\n";
+                  *_out << std::string(31, ' ') << "BIDS" << std::string(31, ' ') << " | " << std::string(31, ' ') << "ASKS" << std::string(31, ' ') << "\n"
+                        << std::left << std::setw(26) << "TOTAL" << std::setw(20) << "QUANTITY" << std::right << std::setw(20) << "PRICE"
+                        << " | " << std::left << std::setw(17) << "PRICE" << std::right << std::setw(23) << "QUANTITY" << std::setw(26) << "TOTAL" << "\n"
+                        << std::string(135, '-') << "\n";
 
                   vector<market_order>::iterator bid_itr = bids_asks.first.begin();
                   auto ask_itr = bids_asks.second.begin();
@@ -946,7 +946,7 @@ namespace bts { namespace cli {
                       *_out << std::left << std::setw(26) << (bid_itr->type == bts::blockchain::bid_order?
                                  _client->get_chain()->to_pretty_asset(bid_itr->get_balance())
                                : _client->get_chain()->to_pretty_asset(bid_itr->get_quantity()))
-                            << std::setw(14) << (bid_itr->type == bts::blockchain::bid_order?
+                            << std::setw(20) << (bid_itr->type == bts::blockchain::bid_order?
                                  _client->get_chain()->to_pretty_asset(bid_itr->get_quantity())
                                : _client->get_chain()->to_pretty_asset(bid_itr->get_balance()))
                             << std::right << std::setw(20) << _client->get_chain()->to_pretty_price(bid_itr->get_price());
@@ -958,13 +958,15 @@ namespace bts { namespace cli {
 
                       ++bid_itr;
                     }
+                    else
+                      *_out << std::string(67, ' ');
 
                     *_out << "| ";
 
                     if( ask_itr != bids_asks.second.end() )
                     {
-                      *_out << std::left << std::setw(14) << _client->get_chain()->to_pretty_price(ask_itr->get_price())
-                            << std::right << std::setw(20) << _client->get_chain()->to_pretty_asset(ask_itr->get_balance())
+                      *_out << std::left << std::setw(17) << _client->get_chain()->to_pretty_price(ask_itr->get_price())
+                            << std::right << std::setw(23) << _client->get_chain()->to_pretty_asset(ask_itr->get_balance())
                             << std::right << std::setw(26) << _client->get_chain()->to_pretty_asset(ask_itr->get_balance()*ask_itr->get_price());
                       ++ask_itr;
                     }
