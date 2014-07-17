89b2206d75a90c11796928beb690d63fac7f3267
Vikram Rajkumar <vikram@bitshares.org>
2014-07-16 21:34:31 -0400

    Tweak warning

--- libraries/blockchain/include/bts/blockchain/config.hpp
+++ libraries/blockchain/include/bts/blockchain/config.hpp
@@ -95,8 +95,9 @@
  *
  *   Currently set to 1% of the share in the DAC, or 0.5% for each side of the market.
  */
-#define BTS_BLOCKCHAIN_MARKET_DEPTH_REQUIREMENT     (BTS_BLOCKCHAIN_INITIAL_SHARES/10000) 
-#warning [TEST] revert depth before launch!
+//#define BTS_BLOCKCHAIN_MARKET_DEPTH_REQUIREMENT             (BTS_BLOCKCHAIN_INITIAL_SHARES/100)
+#warning [TEST] Revert market depth requirement before launching!
+#define BTS_BLOCKCHAIN_MARKET_DEPTH_REQUIREMENT             (BTS_BLOCKCHAIN_INITIAL_SHARES/10000)
 
 /**
  *  The number of blocks expected per hour based upon the BTS_BLOCKCHAIN_BLOCK_INTERVAL_SEC
