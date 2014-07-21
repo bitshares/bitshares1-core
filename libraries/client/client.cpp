fc099df476f68b99c0a1517d7e2945052de282bb
Nathan Hourt <nathan@bitshares.org>
2014-07-21 16:50:53 -0400

    account
    With TITAN, it is possible to send anonymously or from a different
    account than is paying, as long as the sender holds the private key of
    the sending account. This commit adds experimental support for this,
    though it has not yet been tested.

--- libraries/client/client.cpp
+++ libraries/client/client.cpp
@@ -1561,6 +1561,25 @@ config load_config( const fc::path& datadir )
     {
          auto trx = _wallet->transfer_asset( amount_to_transfer, asset_symbol,
                                                   from_account_name, to_account_name,
+                                                  to_account_name,
+                                                  memo_message, true );
+
+         network_broadcast_transaction( trx );
+
+         return trx;
+    }
+
+    signed_transaction detail::client_impl::wallet_transfer_from(double amount_to_transfer,
+                                                       const string& asset_symbol,
+                                                       const string& paying_account_name,
+                                                       const string& from_account_name,
+                                                       const string& to_account_name,
+                                                       const string& memo_message)
+    {
+         auto trx = _wallet->transfer_asset( amount_to_transfer, asset_symbol,
+                                                  paying_account_name,
+                                                  from_account_name, to_account_name,
+                                                  to_account_name,
                                                   memo_message, true );
 
          network_broadcast_transaction( trx );
--- libraries/wallet/include/bts/wallet/wallet.hpp
+++ libraries/wallet/include/bts/wallet/wallet.hpp
@@ -254,8 +254,9 @@ namespace bts { namespace wallet {
           *  This transfer works like a bitcoin transaction combining multiple inputs
           *  and producing a single output.
           */
-         signed_transaction  transfer_asset( double real_amount_to_transfer,
+         signed_transaction  transfer_asset(double real_amount_to_transfer,
                                               const string& amount_to_transfer_symbol,
+                                              const string& paying_account_name,
                                               const string& from_account_name,
                                               const string& to_account_name,
                                               const string& memo_message,
