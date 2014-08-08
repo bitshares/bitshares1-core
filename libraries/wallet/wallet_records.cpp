3f7ac2ce266a4939b7f92db48099918d7debfc83
Daniel Larimer <dlarimer@invictus-innovations.com>
2014-08-08 10:27:39 -0400

    Ticket #609 - adding extra sanity checks around key storage/retrevial

--- libraries/wallet/wallet_records.cpp
+++ libraries/wallet/wallet_records.cpp
@@ -5,6 +5,7 @@ namespace bts { namespace wallet {
 
    extended_private_key master_key::decrypt_key( const fc::sha512& password )const
    { try {
+      FC_ASSERT( password != fc::sha512() );
       FC_ASSERT( encrypted_key.size() );
       auto plain_text = fc::aes_decrypt( password, encrypted_key );
       return fc::raw::unpack<extended_private_key>( plain_text );
@@ -18,8 +19,13 @@ namespace bts { namespace wallet {
    void master_key::encrypt_key( const fc::sha512& password, 
                                  const extended_private_key& k )
    { try {
+      FC_ASSERT( password != fc::sha512() );
       checksum = fc::sha512::hash( password );
       encrypted_key = fc::aes_encrypt( password, fc::raw::pack( k ) );
+
+      // just to double check... we should find out if there is
+      // a problem ASAP...
+      decrypt_key( password );
    } FC_CAPTURE_AND_RETHROW() }
 
    bool key_data::has_private_key()const 
@@ -30,12 +36,17 @@ namespace bts { namespace wallet {
    void key_data::encrypt_private_key( const fc::sha512& password, 
                                         const fc::ecc::private_key& key_to_encrypt )
    { try {
+      FC_ASSERT( password != fc::sha512() );
       public_key            = key_to_encrypt.get_public_key();
       encrypted_private_key = fc::aes_encrypt( password, fc::raw::pack( key_to_encrypt ) );
+
+      // just to double check, we should find out if there is a problem ASAP
+      FC_ASSERT( key_to_encrypt == decrypt_private_key( password ) );
    } FC_CAPTURE_AND_RETHROW() }
 
    fc::ecc::private_key key_data::decrypt_private_key( const fc::sha512& password )const
    { try {
+      FC_ASSERT( password != fc::sha512() );
       auto plain_text = fc::aes_decrypt( password, encrypted_private_key );
       return fc::raw::unpack<fc::ecc::private_key>( plain_text );
    } FC_CAPTURE_AND_RETHROW() }
