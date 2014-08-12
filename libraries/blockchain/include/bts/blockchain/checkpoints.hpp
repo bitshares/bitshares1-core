845905f54d5b4bac66e8ec1c57e3ea0eb341f6fc
bitsha256 <bitsha256@gmail.com>
2014-08-12 16:12:23 -0400

    hard fork number and add checkpoint

--- libraries/blockchain/include/bts/blockchain/checkpoints.hpp
+++ libraries/blockchain/include/bts/blockchain/checkpoints.hpp
@@ -5,4 +5,6 @@
 #include <bts/blockchain/types.hpp>
 
 const static std::map<uint32_t, bts::blockchain::block_id_type> CHECKPOINT_BLOCKS {
+    {1, bts::blockchain::block_id_type("8abcfb93c52f999e3ef5288c4f837f4f15af5521")},
+    {206632, bts::blockchain::block_id_type("565ab0f92e3af4369d11ecd0ea0b95c93eb74a1c")}
 };
--- libraries/blockchain/include/bts/blockchain/fork_blocks.hpp
+++ libraries/blockchain/include/bts/blockchain/fork_blocks.hpp
@@ -5,4 +5,4 @@
 /** @file bts/blockchain/fork_blocks.hpp
  *  @brief Defines global constants that hard forks happen
  */
-#define BTS_BLOCKCHAIN_FORK_MARKET_BLOCK_NUM                  (200000)
+#define BTS_BLOCKCHAIN_FORK_MARKET_BLOCK_NUM                  (216000)
