2fc50b92305b2b2710bc5ca512ded47115f1bb17
Eric Frias <efrias@syncad.com>
2014-08-15 16:45:40 -0400

    Add notifications when the client loses a connection

---
+++ libraries/client/include/bts/client/notifier.hpp
@@ -0,0 +1,22 @@
+#pragma once
+#include <memory>
+#include <string>
+#include <stdint.h>
+
+namespace bts { namespace client { 
+  namespace detail {
+    class bts_gntp_notifier_impl;
+  }
+
+  class bts_gntp_notifier {
+  public:
+    bts_gntp_notifier();
+    ~bts_gntp_notifier();
+
+    void connection_count_dropped_below_threshold(uint32_t current_connection_count, uint32_t notification_threshold);
+  private:
+    std::unique_ptr<detail::bts_gntp_notifier_impl> my;
+  };
+  typedef std::shared_ptr<bts_gntp_notifier> bts_gntp_notifier_ptr;
+
+} } // end namespace bts::client
