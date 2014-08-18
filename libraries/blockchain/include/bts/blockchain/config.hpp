e1dec2da74d3f79b7970dd6bb98f94f9285704e2
Daniel Larimer <dlarimer@invictus-innovations.com>
2014-08-18 15:37:35 +0000

    bump test net iteration

--- libraries/blockchain/include/bts/blockchain/config.hpp
+++ libraries/blockchain/include/bts/blockchain/config.hpp
@@ -4,7 +4,7 @@
 
 /* Set to true only for test network */
 #define BTS_TEST_NETWORK                                    true
-#define BTS_TEST_NETWORK_VERSION                            16
+#define BTS_TEST_NETWORK_VERSION                            17
 
 /** @file bts/blockchain/config.hpp
  *  @brief Defines global constants that determine blockchain behavior
