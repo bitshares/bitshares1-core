7cf0d0c3e6c3868c6696ef9dc3ede5e780287599
bitsha256 <bitsha256@gmail.com>
2014-08-21 15:31:52 -0400

    bump checkpoint

--- libraries/blockchain/include/bts/blockchain/checkpoints.hpp
+++ libraries/blockchain/include/bts/blockchain/checkpoints.hpp
@@ -6,5 +6,6 @@
 
 const static std::map<uint32_t, bts::blockchain::block_id_type> CHECKPOINT_BLOCKS {
     {1, bts::blockchain::block_id_type("8abcfb93c52f999e3ef5288c4f837f4f15af5521")},
-    {225000, bts::blockchain::block_id_type("2e09195c3e4ef6d58736151ea22f78f08556e6a9")}
+    {225000, bts::blockchain::block_id_type("2e09195c3e4ef6d58736151ea22f78f08556e6a9")},
+    {282000, bts::blockchain::block_id_type("611197d4a3f90f5c9009559b896f0f5d4a4f4b5f")}
 };
