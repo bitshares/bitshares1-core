7e7c49cee54d0f70057c8e2cbe89e07ba3b5832e
bitsha256 <bitsha256@gmail.com>
2014-08-27 16:44:18 -0400

    add linux shell disable qt_wallet

---
+++ HelperScripts/jenkins_build_linux.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -xe
+cd $WORKSPACE/bitsharesx
+git submodule init
+git submodule update
+mkdir $WORKSPACE/build
+cd $WORKSPACE/build
+export BITSHARES_ROOT=$WORKSPACE
+. ../bitsharesx/setenv.sh
+cmake -DINCLUDE_QT_WALLET=OFF -DCMAKE_TOOLCHAIN_FILE=$WORKSPACE/toolchain.invictus/toolchain.invictus.cmake ../bitsharesx
+make -j8
