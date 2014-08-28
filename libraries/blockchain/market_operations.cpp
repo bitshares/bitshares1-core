1589b555fbf05349f4ab539b84fae80763b914bc
Daniel Larimer <dlarimer@invictus-innovations.com>
2014-08-28 17:43:50 -0400

    option to use median feeds

--- libraries/blockchain/market_operations.cpp
+++ libraries/blockchain/market_operations.cpp
@@ -143,7 +143,14 @@ namespace bts { namespace blockchain {
 
       if( market_stat->avg_price_1h.quote_asset_id != 0 )
       {
-         FC_ASSERT( short_index.order_price < market_stat->maximum_bid(), "", ("order",*this)("market_stat",market_stat) );
+         if( eval_state._current_state->get_head_block_num() > BTS_BLOCKCHAIN_NEW_SHORTS_LIMIT_TO_1HR_AVG )
+         {
+            FC_ASSERT( short_index.order_price < market_stat->avg_price_1h, "", ("order",*this)("market_stat",market_stat) );
+         }
+         else
+         {
+            FC_ASSERT( short_index.order_price < market_stat->maximum_bid(), "", ("order",*this)("market_stat",market_stat) );
+         }
       }
       else
       {
