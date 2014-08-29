7be6937d3821e929bfebff11401730e526ef8f90
Gandalf <gandalf@storm.pl>
2014-08-29 10:46:40 +0200

    Added QT/ICU to setenv.sh

--- setenv.sh
+++ setenv.sh
@@ -12,7 +12,11 @@ export TOOLCHAIN_ROOT=$BITSHARES_ROOT/toolchain.invictus
 export PKG_CONFIG_SYSROOT_DIR=$TOOLCHAIN_ROOT/x86_64-unknown-linux-gnu/sysroot
 export PKG_CONFIG_PATH=$TOOLCHAIN_ROOT/x86_64-unknown-linux-gnu/sysroot/usr/lib/pkgconfig
 
-export PATH=$WORKSPACE/build/bin:$WORKSPACE/build/programs/utils:$WORKSPACE/build/libraries/api:$PATH
+export QTDIR=$INVICTUS_ROOT/QT 
+echo "Using: "$QTDIR "as QTDIR"
+export ICUROOT=$INVICTUS_ROOT/ICU
+
+export PATH=$WORKSPACE/build/bin:$QTDIR/bin:$WORKSPACE/build/programs/utils:$WORKSPACE/build/libraries/api:$PATH
 
 export OPENSSL_ROOT=$BITSHARES_ROOT/openssl
 export OPENSSL_ROOT_DIR=$OPENSSL_ROOT
