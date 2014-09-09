52468730594358d16f63f4140218c6e617e50b46
Vikram Rajkumar <vikram@bitshares.org>
2014-09-09 14:41:21 -0400

    Update checkpoint

--- libraries/blockchain/include/bts/blockchain/checkpoints.hpp
+++ libraries/blockchain/include/bts/blockchain/checkpoints.hpp
@@ -1,11 +1,10 @@
 #pragma once
-
+#include <bts/blockchain/types.hpp>
 #include <map>
 
-#include <bts/blockchain/types.hpp>
-
-const static std::map<uint32_t, bts::blockchain::block_id_type> CHECKPOINT_BLOCKS {
-    {1, bts::blockchain::block_id_type("8abcfb93c52f999e3ef5288c4f837f4f15af5521")},
-    {225000, bts::blockchain::block_id_type("2e09195c3e4ef6d58736151ea22f78f08556e6a9")},
-    {434800, bts::blockchain::block_id_type("f3cd2d104efea2c36583d62a3887251194be5c43")}
+const static std::map<uint32_t, bts::blockchain::block_id_type> CHECKPOINT_BLOCKS
+{
+    {      1, bts::blockchain::block_id_type( "8abcfb93c52f999e3ef5288c4f837f4f15af5521" ) },
+    { 225000, bts::blockchain::block_id_type( "2e09195c3e4ef6d58736151ea22f78f08556e6a9" ) },
+    { 442700, bts::blockchain::block_id_type( "c22e2dc1954f7d3a9620048d64587018e98c03f8" ) }
 };
