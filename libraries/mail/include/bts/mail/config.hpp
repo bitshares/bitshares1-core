70802e1a24f2285f694bb2da3af21c6818463596
Nathan Hourt <nat.hourt@gmail.com>
2014-10-17 13:13:16 -0400

    Default mail server to seed node

--- libraries/mail/include/bts/mail/config.hpp
+++ libraries/mail/include/bts/mail/config.hpp
@@ -4,5 +4,4 @@
 #define BTS_MAIL_MAX_MESSAGE_SIZE_BYTES (1024*1024)
 #define BTS_MAIL_MAX_MESSAGE_AGE (fc::minutes(5))
 #define BTS_MAIL_PROOF_OF_WORK_TARGET (fc::ripemd160("000000fffdeadbeeffffffffffffffffffffffff"))
-#define BTS_MAIL_DEFAULT_MAIL_SERVERS (std::unordered_set<fc::ip::endpoint>({fc::ip::endpoint(fc::ip::address("127.0.0.1"), 3000)}))
-//#define BTS_MAIL_DEFAULT_MAIL_SERVERS (std::unordered_set<fc::ip::endpoint>({fc::ip::endpoint(fc::ip::address("107.170.30.182"), 1800)}))
+#define BTS_MAIL_DEFAULT_MAIL_SERVERS (std::unordered_set<fc::ip::endpoint>({fc::ip::endpoint(fc::ip::address("107.170.30.182"), 1800)}))
