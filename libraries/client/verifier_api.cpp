d42fff512a6bb685648f302c669c12a2da32d83e
Nathan Hourt <nat.hourt@gmail.com>
2014-10-30 16:22:04 -0400

    Add ID Verifier API
    No implementation yet; just create the API

---
+++ libraries/client/verifier_api.cpp
@@ -0,0 +1,12 @@
+#include <bts/client/client.hpp>
+#include <bts/client/client_impl.hpp>
+#include <bts/mail/message.hpp>
+
+namespace bts { namespace client { namespace detail {
+
+mail::message client_impl::verifier_store_new_request(const mail::message& request)
+{
+
+}
+
+} } } // namespace bts::client::detail
