6c2f7afbc95e779bd71b9f4592cbeeab94210744
Vikram Rajkumar <vikram@bitshares.org>
2014-11-07 16:45:58 -0500

    Add wallet_check_sharedrop

--- libraries/blockchain/asset_operations.cpp
+++ libraries/blockchain/asset_operations.cpp
@@ -4,6 +4,32 @@
 
 namespace bts { namespace blockchain {
 
+   bool create_asset_operation::is_power_of_ten( int64_t n )
+   {
+      switch( n )
+      {
+         case 1ll:
+         case 10ll:
+         case 100ll:
+         case 1000ll:
+         case 10000ll:
+         case 100000ll:
+         case 1000000ll:
+         case 10000000ll:
+         case 100000000ll:
+         case 1000000000ll:
+         case 10000000000ll:
+         case 100000000000ll:
+         case 1000000000000ll:
+         case 10000000000000ll:
+         case 100000000000000ll:
+         case 1000000000000000ll:
+            return true;
+         default:
+            return false;
+      }
+   }
+
    /**
     *  @note in this method we are using 'this->' to refer to member variables for
     *  clarity.
