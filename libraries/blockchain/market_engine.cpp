ceb2290f2649f6bbf4c066ef5b2f526ed4b20a49
Daniel Larimer <dlarimer@invictus-innovations.com>
2014-11-07 02:30:31 -0500

    Fix margin call on expired position.
    The while loop was supposed to iterate over all cover positions looking
    for expired orders to force them to cover. This is not the most
    effecient approach, but it will work.

--- libraries/blockchain/market_engine.cpp
+++ libraries/blockchain/market_engine.cpp
@@ -630,6 +630,8 @@ namespace bts { namespace blockchain { namespace detail {
                 --_collateral_itr;
                 return _current_ask.valid();
             }
+            --_collateral_itr;
+            continue;
         }
         _collateral_itr.reset();
         break;
