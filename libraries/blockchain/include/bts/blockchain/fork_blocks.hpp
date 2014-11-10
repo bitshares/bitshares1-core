09b31153a97332460a6e0ce18f6b6a7a48d97582
Vikram Rajkumar <vikram@bitshares.org>
2014-11-10 15:21:10 -0500

    Fix chain id

--- libraries/blockchain/include/bts/blockchain/fork_blocks.hpp
+++ libraries/blockchain/include/bts/blockchain/fork_blocks.hpp
@@ -7,7 +7,7 @@
 #include <stdint.h>
 #include <vector>
 
-#define BTS_EXPECTED_CHAIN_ID                   digest_type( "a3ae3e4507e86bc992296eabdc170f0c0bacde3b5dd51d3d0b3380ad51e9d053" )
+#define BTS_EXPECTED_CHAIN_ID                   digest_type( "0ad5063f9e11a74a7b8120cb22250844b082c562d9c5886c9a593ff9fb083fc1" )
 #define BTS_DESIRED_CHAIN_ID                    digest_type( "75c11a81b7670bbaa721cc603eadb2313756f94a3bcbb9928e9101432701ac5f" )
 
 #define BTS_V0_4_0_FORK_BLOCK_NUM   274000
