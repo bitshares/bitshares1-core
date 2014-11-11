023d0f24c16707b22f8eed4a205e52fa009915e9
Vikram Rajkumar <vikram@bitshares.org>
2014-11-11 15:48:21 -0500

    Cleanup asset op validation logic

--- libraries/wallet/wallet.cpp
+++ libraries/wallet/wallet.cpp
@@ -3356,9 +3356,9 @@ namespace detail {
    asset wallet::get_transaction_fee( const asset_id_type& desired_fee_asset_id )const
    { try {
       FC_ASSERT( is_open() );
-      // TODO: support price conversion using price from blockchain
 
       asset xts_fee( BTS_WALLET_DEFAULT_TRANSACTION_FEE, 0 );
+
       try
       {
           xts_fee = my->_wallet_db.get_property( default_transaction_priority_fee ).as<asset>();
@@ -3369,22 +3369,23 @@ namespace detail {
 
       if( desired_fee_asset_id != 0 )
       {
-         const auto asset_rec = my->_blockchain->get_asset_record( desired_fee_asset_id );
+         const oasset_record asset_rec = my->_blockchain->get_asset_record( desired_fee_asset_id );
          FC_ASSERT( asset_rec.valid() );
          if( asset_rec->is_market_issued() )
          {
-             auto median_price = my->_blockchain->get_median_delegate_price( desired_fee_asset_id );
-             if( median_price )
+             const oprice median_price = my->_blockchain->get_median_delegate_price( desired_fee_asset_id );
+             if( median_price.valid() )
              {
                 xts_fee += xts_fee + xts_fee;
                 // fees paid in something other than XTS are discounted 50%
-                auto alt_fees_paid = xts_fee * *median_price;
+                const asset alt_fees_paid = xts_fee * *median_price;
                 return alt_fees_paid;
              }
          }
       }
+
       return xts_fee;
-       } FC_CAPTURE_AND_RETHROW() }
+   } FC_CAPTURE_AND_RETHROW( (desired_fee_asset_id) ) }
 
    bool wallet::asset_can_pay_fee(const asset_id_type& desired_fee_asset_id) const
    {
