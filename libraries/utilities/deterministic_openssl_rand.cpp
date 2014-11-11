ac9bd6925600b78d0643a57f260ee1441f3931bb
drltc <drltc@users.noreply.github.com>
2014-11-11 17:41:38 -0500

    Move deterministic_openssl_rand from tests to libraries/utilities
    - Clarify method names
    - Make a couple more things static

--- libraries/utilities/deterministic_openssl_rand.cpp
+++ libraries/utilities/deterministic_openssl_rand.cpp
@@ -12,15 +12,16 @@
 #include <openssl/rand.h>
 
 // These don't need to do anything if you don't have anything for them to do.
-static void stdlib_rand_cleanup() {}
-static void stdlib_rand_add(const void *buf, int num, double add_entropy) {}
-static int stdlib_rand_status() { return 1; }
-static void stdlib_rand_seed(const void *buf, int num) {}
+static void deterministic_rand_cleanup() {}
+static void deterministic_rand_add(const void *buf, int num, double add_entropy) {}
+static int  deterministic_rand_status() { return 1; }
+static void deterministic_rand_seed(const void *buf, int num) {}
 
 static fc::sha512 seed;
-static int stdlib_rand_bytes(unsigned char *buf, int num)
-{
 
+static int  deterministic_rand_bytes(unsigned char *buf, int num)
+{
+  std::cout << "deterministic_rand_bytes()\n";
   while (num)
   {
     seed = fc::sha512::hash(seed);
@@ -34,20 +35,22 @@ static int stdlib_rand_bytes(unsigned char *buf, int num)
 }
 
 // Create the table that will link OpenSSL's rand API to our functions.
-RAND_METHOD stdlib_rand_meth = {
-        stdlib_rand_seed,
-        stdlib_rand_bytes,
-        stdlib_rand_cleanup,
-        stdlib_rand_add,
-        stdlib_rand_bytes,
-        stdlib_rand_status
+static RAND_METHOD deterministic_rand_vtable = {
+        deterministic_rand_seed,
+        deterministic_rand_bytes,
+        deterministic_rand_cleanup,
+        deterministic_rand_add,
+        deterministic_rand_bytes,
+        deterministic_rand_status
 };
 
-// This is a public-scope accessor method for our table.
-RAND_METHOD *RAND_stdlib() { return &stdlib_rand_meth; }
+namespace bts { namespace utilities {
 
 void set_random_seed_for_testing(const fc::sha512& new_seed)
 {
-  RAND_set_rand_method(RAND_stdlib());
-  seed = new_seed;
+    RAND_set_rand_method(&deterministic_rand_vtable);
+    seed = new_seed;
+    return;
 }
+
+} }
