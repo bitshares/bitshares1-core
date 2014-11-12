3ef31582552a1d3f95a067089812e0d627049ee6
Nikolai Mushegian <nikolai.mushegian@gmail.com>
2014-11-12 15:25:09 -0500

    btc api work #953

--- libraries/client/btc_api.cpp
+++ libraries/client/btc_api.cpp
@@ -75,12 +75,29 @@ address   detail::client_impl::btc_importprivkey( const string& real_account,
 } FC_RETHROW_EXCEPTIONS( warn, "" ) }
 
 
-std::vector<std::string>   detail::client_impl::btc_listaccounts( const string& real_account,
-                                                                  const string& asset_symbol ) const
+std::vector<string>   detail::client_impl::btc_listaccounts( const string& real_account,
+                                                             const string& asset_symbol ) const
 { try {
-    return std::vector<std::string>();
+    auto pubkeys = _wallet->get_public_keys_in_account( real_account );
+    auto accounts = std::set<string>();
+    for( auto key : pubkeys )
+    {
+        accounts.insert( _wallet->get_address_group_label( address(key) ) );
+    }
+    return std::vector<string>(accounts.begin(), accounts.end());
 } FC_RETHROW_EXCEPTIONS( warn, "" ) }
 
+std::map<string, double>  detail::client_impl::btc_listaddressgroupings( const string& real_account,
+                                                                         const string& asset_symbol ) const
+{ try {
+    auto pubkeys = _wallet->get_public_keys_in_account( real_account );
+    auto ret = map<string, double>();
+    for( auto key : pubkeys )
+    {
+        FC_ASSERT(!"unimplemented");
+    }
+    return ret;
+} FC_RETHROW_EXCEPTIONS( warn, "" ) }
 
 
 }}} // bts::client::detail
