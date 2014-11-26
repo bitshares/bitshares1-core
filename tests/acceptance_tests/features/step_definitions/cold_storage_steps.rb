27ab0411e5336f7f341cda9c9a8cc4059a5419c0
Nikolai Mushegian <nikolai.mushegian@gmail.com>
2014-11-25 22:36:46 -0500

    fix test

--- tests/acceptance_tests/features/step_definitions/cold_storage_steps.rb
+++ tests/acceptance_tests/features/step_definitions/cold_storage_steps.rb
@@ -1,5 +1,5 @@
 When(/^I do transfer (\d+) (\w+) from accountname: (\w+) to address: (\w+)$/) do |amount, symbol, account, address|
-    @current_actor.node.exec 'wallet_transfer_asset_to_address', amount, symbol, account, @addresses[address]
+    @current_actor.node.exec 'wallet_transfer_to_address', amount, symbol, account, @addresses[address]
 end
 
 When(/^I do transfer (\d+) (\w+) from address: (\w+) to account: (\w+)$/) do |amount, symbol, address, account|
