2bbf2fb84726c0d9a396ec5034798b4e4e278358
Vikram Rajkumar <vikram@bitshares.org>
2014-12-02 14:58:48 -0500

    BitSharesX -> BitShares renaming in build scripts

--- HelperScripts/jenkins_build.sh
+++ HelperScripts/jenkins_build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -xe
-cd $WORKSPACE/bitsharesx
+cd $WORKSPACE/bitshares
 git submodule init
 git submodule update
 mkdir $WORKSPACE/build
 cd $WORKSPACE/build
 export BITSHARES_ROOT=$WORKSPACE
-. ../bitsharesx/setenv.sh
-cmake -DINCLUDE_QT_WALLET=TRUE -DCMAKE_TOOLCHAIN_FILE=$WORKSPACE/toolchain.invictus/toolchain.invictus.cmake ../bitsharesx
+. ../bitshares/setenv.sh
+cmake -DINCLUDE_QT_WALLET=TRUE -DCMAKE_TOOLCHAIN_FILE=$WORKSPACE/toolchain.invictus/toolchain.invictus.cmake ../bitshares
 make -j8 VERBOSE=1
