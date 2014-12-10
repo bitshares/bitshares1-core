3968a2f01f5086bb2be4a8e51bb82b59fa28d730
Vikram Rajkumar <vikram@bitshares.org>
2014-12-10 10:07:38 -0500

    Allow trading higher MIAs with lower UIAs; #949

--- libraries/blockchain/market_engine.cpp
+++ libraries/blockchain/market_engine.cpp
@@ -62,8 +62,8 @@ namespace bts { namespace blockchain { namespace detail {
           asset trading_volume(0, base_id);
           price opening_price, closing_price;
 
-          if( !_ask_itr.valid() ) _ask_itr = _db_impl._ask_db.begin(); 
-          if( !_relative_ask_itr.valid() ) _relative_ask_itr = _db_impl._relative_ask_db.begin(); 
+          if( !_ask_itr.valid() ) _ask_itr = _db_impl._ask_db.begin();
+          if( !_relative_ask_itr.valid() ) _relative_ask_itr = _db_impl._relative_ask_db.begin();
 
           if( _bid_itr.valid() )   --_bid_itr;
           else _bid_itr = _db_impl._bid_db.last();
@@ -78,8 +78,12 @@ namespace bts { namespace blockchain { namespace detail {
           else _short_itr = _db_impl._short_db.last();
 
           _feed_price = _db_impl.self->get_median_delegate_price( _quote_id, _base_id );
-          // Market issued assets cannot match until the first time there is a median feed
-          if( quote_asset->is_market_issued() && !base_asset->is_market_issued() )
+
+#ifndef WIN32
+#warning [HARDFORK] add this to old version
+#endif
+          // Market issued assets cannot match until the first time there is a median feed; assume feed price base id 0
+          if( quote_asset->is_market_issued() && base_asset->id == asset_id_type( 0 ) )
           {
               const omarket_status market_stat = _pending_state->get_market_status( _quote_id, _base_id );
               if( (!market_stat.valid() || !market_stat->last_valid_feed_price.valid()) && !_feed_price.valid() )
@@ -211,7 +215,7 @@ namespace bts { namespace blockchain { namespace detail {
                 pay_current_short( mtrx, *quote_asset, *base_asset );
                 pay_current_cover( mtrx, *quote_asset );
             }
-            else if( (_current_bid->type == bid_order || _current_bid->type == relative_bid_order) && 
+            else if( (_current_bid->type == bid_order || _current_bid->type == relative_bid_order) &&
                      _current_ask->type == cover_order )
             {
                 const asset cover_collateral          = asset( *_current_ask->collateral, _base_id );
@@ -236,7 +240,7 @@ namespace bts { namespace blockchain { namespace detail {
                 pay_current_bid( mtrx, *quote_asset );
                 pay_current_cover( mtrx, *quote_asset );
             }
-            else if( (_current_ask->type == ask_order || _current_ask->type == relative_bid_order) && 
+            else if( (_current_ask->type == ask_order || _current_ask->type == relative_bid_order) &&
                      _current_bid->type == short_order )
             {
                 // Bound collateral ratio (maximizes collateral of new margin position)
@@ -266,7 +270,7 @@ namespace bts { namespace blockchain { namespace detail {
                 pay_current_short( mtrx, *quote_asset, *base_asset );
                 pay_current_ask( mtrx, *quote_asset );
             }
-            else if( (_current_ask->type == ask_order || _current_ask->type == relative_ask_order) && 
+            else if( (_current_ask->type == ask_order || _current_ask->type == relative_ask_order) &&
                      (_current_bid->type == bid_order || _current_bid->type == relative_bid_order ) )
             {
                 const asset bid_quantity_xts = _current_bid->get_quantity();
@@ -640,7 +644,7 @@ namespace bts { namespace blockchain { namespace detail {
       if( _bid_itr.valid() )
       {
          market_order abs_bid( bid_order, _bid_itr.key(), _bid_itr.value() );
-         if( abs_bid.get_price().quote_asset_id == _quote_id && abs_bid.get_price().base_asset_id == _base_id ) 
+         if( abs_bid.get_price().quote_asset_id == _quote_id && abs_bid.get_price().base_asset_id == _base_id )
          {
             if( bid )
             {
@@ -718,7 +722,7 @@ namespace bts { namespace blockchain { namespace detail {
        */
       while( _collateral_expiration_itr != _db_impl._collateral_expiration_index.end() )
       {
-         if( _collateral_expiration_itr->quote_id != _quote_id ) 
+         if( _collateral_expiration_itr->quote_id != _quote_id )
             break;
 
          if( _collateral_expiration_itr->expiration > fc::time_point(_pending_state->now()) )
@@ -735,7 +739,7 @@ namespace bts { namespace blockchain { namespace detail {
          ++_collateral_expiration_itr;
 
          // if we have a feed price and margin was called above then don't process it
-         if( !(_feed_price.valid() && cover_ask.get_price() > *_feed_price) )  
+         if( !(_feed_price.valid() && cover_ask.get_price() > *_feed_price) )
          {
             _current_ask = cover_ask;
             return true;
