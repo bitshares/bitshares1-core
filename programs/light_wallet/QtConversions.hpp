92f66cb64d881768d4e9085ef343686f1fe9928f
Nathan Hourt <nat.hourt@gmail.com>
2014-12-16 14:58:33 -0500

    Light Wallet: add support for creating wallet

--- programs/light_wallet/QtConversions.hpp
+++ programs/light_wallet/QtConversions.hpp
@@ -8,20 +8,20 @@
 
 #include <string>
 
-inline const static fc::time_point_sec convert(const QDateTime& t)
+inline static fc::time_point_sec convert(const QDateTime& t)
 {
    return fc::time_point_sec(t.toTime_t());
 }
-inline const static QDateTime convert(const fc::time_point_sec& t)
+inline static QDateTime convert(const fc::time_point_sec& t)
 {
    return QDateTime::fromTime_t(t.sec_since_epoch());
 }
 
-inline const static std::string convert(const QString& s)
+inline static std::string convert(const QString& s)
 {
    return s.toStdString();
 }
-inline const static QString convert(const std::string& s)
+inline static QString convert(const std::string& s)
 {
    return QString::fromStdString(s);
 }
