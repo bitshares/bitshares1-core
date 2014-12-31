66a0c6af76db41fd7c9ff63270ce5e58cd1d823f
Vikram Rajkumar <vikram@bitshares.org>
2014-12-30 23:57:51 -0500

    Handle asset symbol hardfork

--- libraries/blockchain/chain_interface_v1.cpp
+++ libraries/blockchain/chain_interface_v1.cpp
@@ -1,10 +1,38 @@
 #include <bts/blockchain/chain_interface.hpp>
 #include <bts/blockchain/exceptions.hpp>
+#include <locale>
+
+#define ASSET_MAX_SYMBOL_SIZE 8
+#define ASSET_MIN_SYMBOL_SIZE 3
 
 #define BTS_BLOCKCHAIN_DELEGATE_REGISTRATION_FEE    (BTS_BLOCKCHAIN_BLOCKS_PER_DAY / 12)
 #define BTS_BLOCKCHAIN_ASSET_REGISTRATION_FEE       (BTS_BLOCKCHAIN_BLOCKS_PER_DAY * 14)
 
 namespace bts { namespace blockchain {
+
+bool chain_interface::is_valid_symbol_name_v1( const string& symbol )const
+{ try {
+    FC_ASSERT( symbol != "BTSX" );
+
+    if( symbol.size() < ASSET_MIN_SYMBOL_SIZE || symbol.size() > ASSET_MAX_SYMBOL_SIZE )
+        return false;
+
+    int dots = 0;
+    for( const char c : symbol )
+    {
+        if( c == '.' )
+        {
+           if( ++dots > 1 )
+            return false;
+        }
+        else if( !std::isalnum( c, std::locale::classic() ) || !std::isupper( c, std::locale::classic() ) )
+            return false;
+    }
+    if( symbol.back() == '.' ) return false;
+    if( symbol.front() == '.' ) return false;
+
+    return true;
+} FC_CAPTURE_AND_RETHROW( (symbol) ) }
 
 share_type chain_interface::get_delegate_registration_fee_v1( uint8_t pay_rate )const
 {
