4f69f0093a3b50e745359c327cd2a3beede6d1ad
Vikram Rajkumar <vikram@bitshares.org>
2015-01-07 14:45:47 -0500

    Restore DevShares changes

--- libraries/mail/include/bts/mail/config.hpp
+++ libraries/mail/include/bts/mail/config.hpp
@@ -4,4 +4,4 @@
 #define BTS_MAIL_MAX_MESSAGE_SIZE_BYTES (1024*1024)
 #define BTS_MAIL_MAX_MESSAGE_AGE (fc::minutes(5))
 #define BTS_MAIL_PROOF_OF_WORK_TARGET (fc::ripemd160("000ffffffdeadbeeffffffffffffffffffffffff"))
-#define BTS_MAIL_DEFAULT_MAIL_SERVERS (std::unordered_set<std::string>({"nathanhourt.com"}))
+#define BTS_MAIL_DEFAULT_MAIL_SERVERS (std::unordered_set<std::string>())
