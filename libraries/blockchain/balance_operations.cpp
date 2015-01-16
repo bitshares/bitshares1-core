f8f9de7d7a9395c9334beae57c6c508556b61e72
Vikram Rajkumar <vikram@bitshares.org>
2015-01-15 19:54:27 -0500

    Revert "Add merge warning"
    This reverts commit 9221c74aed8344548e00275698cee678284cd5aa.

--- libraries/blockchain/balance_operations.cpp
+++ libraries/blockchain/balance_operations.cpp
@@ -555,9 +555,7 @@ namespace bts { namespace blockchain {
 
    } FC_CAPTURE_AND_RETHROW( (*this) ) }
 
-#ifndef WIN32
-#warning [VIKRAM] New operation fork
-#endif
+
    void pay_fee_operation::evaluate( transaction_evaluation_state& eval_state )
    {
       eval_state._max_fee[this->amount.asset_id] += this->amount.amount;
