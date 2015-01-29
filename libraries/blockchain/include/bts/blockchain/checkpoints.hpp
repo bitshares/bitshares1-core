c760458f86ac64fd443b5e115d0eb627a71d7221
Vikram Rajkumar <vikram@bitshares.org>
2015-01-29 12:06:21 -0500

    Update checkpoint

--- libraries/blockchain/include/bts/blockchain/checkpoints.hpp
+++ libraries/blockchain/include/bts/blockchain/checkpoints.hpp
@@ -8,7 +8,7 @@ static std::map<uint32_t, bts::blockchain::block_id_type> CHECKPOINT_BLOCKS
     {       1, bts::blockchain::block_id_type( "8523e28fde6eed4eb749a84e28c9c7b56870c9cc" ) },
     {  172718, bts::blockchain::block_id_type( "e452dd44902bc86110923285da03ac12e8cfbe6f" ) },
     {  173585, bts::blockchain::block_id_type( "1d1695bc8ebd3ebae0168007afb2912269adbef1" ) },
-    {  179000, bts::blockchain::block_id_type( "ba3d03f5bf65d8cc051417b673206e6b0472bcae" ) }
+    {  192000, bts::blockchain::block_id_type( "80910335f384f420930351deac69debd007f74c0" ) }
 };
 
 } } // bts::blockchain
