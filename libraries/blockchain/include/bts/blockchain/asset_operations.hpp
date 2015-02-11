d85ebcd9cbe5420b436ad7ce0f49b193bc72dee2
Vikram Rajkumar <vikram@bitshares.org>
2015-02-11 15:58:35 -0500

    Clean out UIA proposal code for now

--- libraries/blockchain/include/bts/blockchain/asset_operations.hpp
+++ libraries/blockchain/include/bts/blockchain/asset_operations.hpp
@@ -52,16 +52,6 @@ namespace bts { namespace blockchain {
        void evaluate( transaction_evaluation_state& eval_state )const;
    };
 
-   struct create_asset_proposal
-   {
-      static const operation_type_enum type;
-
-      asset_id_type  asset_id = 0;
-      object_id_type info = 0;
-
-      void evaluate( transaction_evaluation_state& eval_state )const;
-   };
-
    /**
     * This operation updates an existing issuer record provided
     * it is signed by a proper key.
@@ -98,9 +88,9 @@ namespace bts { namespace blockchain {
        uint32_t           issuer_permissions = -1;
        account_id_type    issuer_account_id;
        uint16_t           market_fee = 0;
-       
+
        /**
-        *  The issuer can specify a transaction fee (of the asset type) 
+        *  The issuer can specify a transaction fee (of the asset type)
         *  that will be paid to the issuer with every transaction that
         *  references this asset type.
         */
@@ -150,8 +140,8 @@ namespace bts { namespace blockchain {
 FC_REFLECT( bts::blockchain::authorize_operation,
             (asset_id)
             (owner)
-            (meta_id)  )
-
+            (meta_id)
+            )
 FC_REFLECT( bts::blockchain::create_asset_operation,
             (symbol)
             (name)
@@ -169,8 +159,7 @@ FC_REFLECT( bts::blockchain::update_asset_operation,
             (maximum_share_supply)
             (precision)
             )
-
-FC_REFLECT_DERIVED( bts::blockchain::update_asset_ext_operation, 
+FC_REFLECT_DERIVED( bts::blockchain::update_asset_ext_operation,
                     (bts::blockchain::update_asset_operation),
                     (flags)
                     (issuer_permissions)
@@ -178,9 +167,6 @@ FC_REFLECT_DERIVED( bts::blockchain::update_asset_ext_operation,
                     (transaction_fee)
                     (market_fee)
                     (authority) )
-
-FC_REFLECT( bts::blockchain::create_asset_proposal, (asset_id)(info) );
-
 FC_REFLECT( bts::blockchain::issue_asset_operation,
             (amount)
             )
