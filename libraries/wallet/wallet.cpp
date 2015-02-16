fce1607093c9fff92c29e12673d4fe9e672f80ec
Vikram Rajkumar <vikram@bitshares.org>
2015-02-15 20:21:12 -0500

    Remove extraneous price descendent

--- libraries/wallet/wallet.cpp
+++ libraries/wallet/wallet.cpp
@@ -1994,7 +1994,6 @@ namespace detail {
            const string& account_to_publish_under,
            double amount_per_xts,
            const string& amount_asset_symbol,
-           bool force_settle,
            bool sign )
    { try {
       FC_ASSERT( is_open() );
@@ -2021,17 +2020,13 @@ namespace detail {
       FC_ASSERT( base_asset_record.valid() );
       FC_ASSERT( quote_asset_record.valid() );
 
-      feed_price quote_price_shares;
       const double ratio = (amount_per_xts * quote_asset_record->precision) / base_asset_record->precision;
-      price& standard_price = quote_price_shares;
-      standard_price = price( ratio, quote_asset_record->id, base_asset_record->id );
-      quote_price_shares.force_settle = force_settle;
+      const price quote_price_shares( ratio, quote_asset_record->id, base_asset_record->id );
 
-      idump( (quote_price_shares) );
       if( amount_per_xts > 0 )
       {
          trx.publish_feed( my->_blockchain->get_asset_id( amount_asset_symbol ),
-                           current_account->id, fc::variant( price( quote_price_shares ) )  );
+                           current_account->id, fc::variant( quote_price_shares ) );
       }
       else
       {
