a72de3b6b398a783d3ed63beec93d077c9bbc215
grzegorzs <gszczepanek@syncad.com>
2015-03-03 14:58:46 +0100

    [GS] Fix issue #1416

--- libraries/cli/cli.cpp
+++ libraries/cli/cli.cpp
@@ -520,6 +520,7 @@ namespace bts { namespace cli {
                        this_parameter.type == "account_name" ||
                        this_parameter.type == "new_account_name" ||
                        this_parameter.type == "method_name" ||
+                       this_parameter.type == "new_passphrase" ||
                        this_parameter.type == "filename" ||
                        this_parameter.type == "public_key" ||
                        this_parameter.type == "order_id" ||
