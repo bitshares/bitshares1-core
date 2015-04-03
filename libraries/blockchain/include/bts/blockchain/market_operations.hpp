6788d4c4da4baa325cfb08802ae1e04df3f87fd2
Vikram Rajkumar <vikram@bitshares.org>
2015-04-03 16:12:04 -0400

    Better record ID generation for virtual market transactions in wallet; resolves #1486

--- libraries/blockchain/include/bts/blockchain/market_operations.hpp
+++ libraries/blockchain/include/bts/blockchain/market_operations.hpp
@@ -5,6 +5,28 @@
 
 namespace bts { namespace blockchain {
 
+struct market_index_key_ext : public market_index_key
+{
+    market_index_key_ext( const market_index_key b = market_index_key(), optional<price> limit = optional<price>() )
+        : market_index_key( b ), limit_price( limit ) {}
+
+    optional<price> limit_price;
+
+    friend bool operator == ( const market_index_key_ext& a, const market_index_key_ext& b )
+    {
+        if( a.limit_price.valid() != b.limit_price.valid() ) return false;
+        if( !a.limit_price.valid() ) return market_index_key( a ) == market_index_key( b );
+        return std::tie( a.order_price, a.owner, *a.limit_price ) == std::tie( b.order_price, b.owner, *b.limit_price );
+    }
+
+    friend bool operator < ( const market_index_key_ext& a, const market_index_key_ext& b )
+    {
+        FC_ASSERT( a.limit_price.valid() == b.limit_price.valid() );
+        if( !a.limit_price.valid() ) return market_index_key( a ) < market_index_key( b );
+        return std::tie( a.order_price, a.owner, *a.limit_price ) < std::tie( b.order_price, b.owner, *b.limit_price );
+    }
+};
+
 struct bid_operation
 {
     static const operation_type_enum type;
@@ -73,7 +95,7 @@ struct add_collateral_operation
     static const operation_type_enum type;
 
     add_collateral_operation(share_type amount = 0, market_index_key cover_index = market_index_key())
-        : amount(amount), cover_index(cover_index){}
+        : amount( amount ), cover_index( cover_index ) {}
 
     asset get_amount()const { return asset( amount, cover_index.order_price.base_asset_id ); }
 
@@ -85,6 +107,7 @@ struct add_collateral_operation
 
 } } // bts::blockchain
 
+FC_REFLECT_DERIVED( bts::blockchain::market_index_key_ext, (bts::blockchain::market_index_key), (limit_price) )
 FC_REFLECT( bts::blockchain::bid_operation, (amount)(bid_index) )
 FC_REFLECT( bts::blockchain::ask_operation, (amount)(ask_index) )
 FC_REFLECT( bts::blockchain::short_operation, (amount)(short_index) )
